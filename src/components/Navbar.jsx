ce864962fd640fdaeaa15f118ee9b196bfd03761
Laith Alwani <laithalwani@gmail.com>
2023-04-09 12:34:55 -0400

    fixes navbar issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,32 +33,31 @@ export default function Navbar() {
         <h3>My Little Shop!</h3>
         <span className="muted">Prices are negotiable</span>
       </div>
-      {user && (
-        <div className="links">
-          {/* {!user && (
+
+      <div className="links">
+        {/* {!user && (
             <Link to="login" className="navLink">
               <MdLogin size={24}/>
             </Link>
           )} */}
-          {user && (
-            <>
-              <Link to="add" className="navLink">
-                <MdPostAdd size={24} />
-              </Link>
-              <Link to="/" onClick={logout} className="navLink">
-                <MdLogout size={24} />
-              </Link>
-            </>
-          )}
-          <a
-            href="https://www.google.com/maps/place/Summerhill+St,+Ottawa,+ON/@45.26683,-75.6979251,17z/data=!3m1!4b1!4m6!3m5!1s0x4ccde31f0d89992b:0xa278a4d7103aff32!8m2!3d45.2668263!4d-75.6953502!16s%2Fg%2F1jkxrrdsf"
-            target="_blank"
-            className="location">
-            <MdLocationOn size={24} />
-            <span>Pickup</span>
-          </a>
-        </div>
-      )}
+        {user && (
+          <>
+            <Link to="add" className="navLink">
+              <MdPostAdd size={24} />
+            </Link>
+            <Link to="/" onClick={logout} className="navLink">
+              <MdLogout size={24} />
+            </Link>
+          </>
+        )}
+        <a
+          href="https://www.google.com/maps/place/Summerhill+St,+Ottawa,+ON/@45.26683,-75.6979251,17z/data=!3m1!4b1!4m6!3m5!1s0x4ccde31f0d89992b:0xa278a4d7103aff32!8m2!3d45.2668263!4d-75.6953502!16s%2Fg%2F1jkxrrdsf"
+          target="_blank"
+          className="location">
+          <MdLocationOn size={32} />
+          <strong>Pickup</strong>
+        </a>
+      </div>
     </nav>
   );
 }
